33e2c84dee59c2dce37d4ae951ff4c18f393763e
catmpat <cp378@st-andrews.ac.uk>
2014-07-20 22:34:06 +0100

    Added jquery to homepage

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+$(document).ready(function(){
+  $("#interests").click(function(){
+    $("#interests-panel").slideToggle("slow");
+    $('body,html').animate({scrollTop: "210px"},"slow");
+  });
+});
+$(document).ready(function(){
+  $("#skills").click(function(){
+    $("#skills-panel").slideToggle("slow");
+    $('body,html').animate({scrollTop: "210px"},"slow");
+  });
+});
+
+
+$(document).ready(function(){
+  $("#experience").click(function(){
+    $("#experience-panel").slideToggle("slow");
+    $('body,html').animate({scrollTop: "210px"},"slow");
+  });
+});
+
