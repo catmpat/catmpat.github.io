728056840cf8e87e46f57d0d0d258dea176394bd
catmpat <cp378@st-andrews.ac.uk>
2014-07-22 23:16:22 +0100

    Main changes to crafts page - clickable

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $("#description").click(function(){
+    $("#description-panel").slideToggle("slow");
+    $('body,html').animate({scrollTop: "210px"},"slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#description1").click(function(){
+    $("#description-panel1").slideToggle("slow");
+    $('body,html').animate({scrollTop: "210px"},"slow");
+  });
+});
